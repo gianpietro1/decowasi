994c91b37853ba73a5b7154e13824edc147620a2
Gianpietro Lavado <gianpietro1@gmail.com>
2016-05-21 22:08:21 -0500

    second version basics

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def admin?
+    role == 'admin'
+  end
+  
+  def professional?
+    role == 'professional'
+  end
+
+  def store?
+    role == 'store'
+  end
+
+  def consumer?
+    role == 'consumer'
+  end
+
+
 end
